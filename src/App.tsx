f120798854c2f3c009004263ff14270818cffcd6
Cory Caywood <cory@floatinginspace.net>
2024-03-24 15:54:12 +0900

    Fix display of file name when url is data uri

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ResourceName from './resources/resource/ResourceName';
 import ResourceContainer from './resources/ResourceContainer';
 import { Resources, emptyResources } from './resources/resource/Resource';
-import renderResource from './resources/render-resource';
+import renderResources from './resources/render-resources';
 import download from './actions/download';
 import getResources from './actions/get-resources';
 
 const tabNames = [ResourceName.stylesheets, ResourceName.scripts, ResourceName.images, ResourceName.fonts];
-// const resources: Resources = {
-//     stylesheets: [
-//         {url: "https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css"}
-//     ],
-//     images: [
-//         {url: "https://i.stack.imgur.com/FdTlD.jpg?s=64&g=1"}
-//     ],
-//     scripts: [
-//         {url: "https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"}
-//     ],
-//     fonts: [
-//         {url: "https://fonts.gstatic.com/s/ibmplexmono/v19/-F6qfjptAgt5VM-kVkqdyU8n3twJwlBFgg.woff2"}
-//     ]
-// }
 
 function App() {
     const [active, setActive] = useState<ResourceName>(tabNames[0])
@@ -31,14 +17,12 @@ function App() {
 
     useEffect(() => {
         getResources().then(res => {
-            console.log(res)
             setResources(res as Resources)
         })
     })
 
-
     return (
-        <ResourceContainer active={active} tabNames={tabNames} resources={renderResource(active, resources, download)} onChangeTab={(name: ResourceName) => setActive(name)} />
+        <ResourceContainer active={active} tabNames={tabNames} resources={renderResources(active, resources, download)} onChangeTab={(name: ResourceName) => setActive(name)} />
     );
 }
 
---
+++ src/resources/render-resources.tsx
@@ -0,0 +1,74 @@
+import React, { ReactNode, CSSProperties } from 'react';
+import { Resources } from './resource/Resource';
+import ResourceName from './resource/ResourceName';
+
+const renderStylesheet = (url: string, onDownloadClick: (url: string, fileName: string) => void) => (
+    <div>
+        <div className="url"><a href={url} target="_blank">{url}</a></div>
+        {renderDownloadLink(url, onDownloadClick)}
+    </div>
+)
+
+const renderImage = (url: string, onDownloadClick: (url: string, fileName: string) => void) => (
+    <div>
+        <img src={url} />
+        {renderDownloadLink(url, onDownloadClick)}
+    </div>
+)
+
+const renderScript = (url: string, onDownloadClick: (url: string, fileName: string) => void) => (
+    <div>
+        <div className="url"><a href={url} target="_blank">{url}</a></div>
+        {renderDownloadLink(url, onDownloadClick)}
+    </div>
+)
+
+const renderFont = (url: string, onDownloadClick: (url: string, fileName: string) => void) => {
+    const fontName = `font-${fileNameFrom(url).split('.')[0]}`
+    const style: CSSProperties = {
+        fontFamily: fontName
+    }
+    return (
+        <div>
+            <style>
+                {`@font-face {
+                    font-family: ${fontName};
+                    src: url(${url})
+                }`}
+            </style>
+            <div className="font-example" style={style}>Grumpy wizards make toxic brew for the evil Queen and Jack.</div>
+            {renderDownloadLink(url, onDownloadClick)}
+        </div>
+    )
+}
+
+const renderDownloadLink = (url: string, onDownloadClick: (url: string, fileName: string) => void) => {
+    const fileName = fileNameFrom(url)
+    return (
+        <div className="url">
+            {fileName} - <a title={url} href={url} onClick={(e) => {e.preventDefault(); onDownloadClick(url, fileName)}}>Download</a>
+        </div>
+    );
+}
+
+const fileNameFrom = (url: string) => url.startsWith('data:') ? url.split(',')[0] :
+    url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"))
+    .substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"))
+    .substring(url.lastIndexOf("/") + 1, url.length)
+
+function renderResources(name: ResourceName, resources: Resources, onDownloadClick: (url: string, fileName: string) => void): ReactNode[] {
+    switch(name) {
+        case ResourceName.stylesheets:
+            return resources.stylesheets.map(stylesheet => renderStylesheet(stylesheet.url, onDownloadClick));
+        case ResourceName.images:
+            return resources.images.map(image => renderImage(image.url, onDownloadClick));
+        case ResourceName.scripts:
+            return resources.scripts.map(script => renderScript(script.url, onDownloadClick));
+        case ResourceName.fonts:
+            return resources.fonts.map(font => renderFont(font.url, onDownloadClick));
+        default:
+            return [<div></div>]
+    }
+}
+
+export default renderResources;
